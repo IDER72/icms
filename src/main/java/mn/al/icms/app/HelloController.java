5d4cf539db3298261747af1b4f7577ae736dd08b
IDER <ider.zorigoo@yahoo.com>
2016-04-13 02:25:18 +0900

    project starting...

---
+++ src/main/java/mn/al/icms/app/HelloController.java
@@ -0,0 +1,14 @@
+package mn.al.icms.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring boot";
+
+	}
+}
---
+++ src/main/java/mn/al/icms/entity/User.java
@@ -0,0 +1,55 @@
+package mn.al.icms.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long value) {
+		this.id = value;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String value) {
+		this.email = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String value) {
+		this.name = value;
+	}
+
+} // class User Status
